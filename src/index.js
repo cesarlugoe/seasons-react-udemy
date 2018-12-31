dc992ef28828fd2b256f1939ee147f67151be442
Cesar Lugo <cesarlugo00@hotmail.com>
2018-12-31 19:08:15 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class App extends Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   state = {
+      isLoading: true,
+      currentPosition: null,
+      errorMessage: ''
+   }
+
+   componentDidMount() {
+      this.setState({
+         isLoading: false,
+      });
+      this.getCurrentPosition()
+   }
+
+   getCurrentPosition() {
+      window.navigator.geolocation.getCurrentPosition(
+         position => this.setState({ currentPosition: position.coords.latitude }),
+         err => this.setState({ errorMessage: err.message })  
+      );
+   }
+
+  render() {
+     const { isLoading, currentPosition, errorMessage } = this.state;
+   
+    return (
+      <div>
+         {isLoading?  <h1>...Loading</h1> : null}
+         { (errorMessage && !currentPosition)? <h1> Error: { errorMessage }</h1> : null }
+         { (!errorMessage && currentPosition)? <h1> Latitude : { currentPosition }</h1> : null }
+      </div>
+    )
+  };
+}
+
+
+ReactDOM.render(
+   <App />, 
+   document.querySelector('#root')
+   );
